76db8406f160ce6b3dbd8d222327f9cf4609628f
naimuddin94 <developernaimuddin@gmail.com>
2024-10-22 22:59:22 +0600

    insert students data

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -29,3 +29,12 @@ CREATE TABLE enrollment(
     course_id INT REFERENCES courses(course_id) ON DELETE CASCADE NOT NULL
 );
 
+-- Insert sample data into the students table
+INSERT INTO students (student_name, age, email, frontend_mark, backend_mark, status)
+VALUES
+('Sameer', 21, 'sameer@example.com', 48, 60, NULL),
+('Zoya', 23, 'zoya@example.com', 52, 58, NULL),
+('Nabil', 22, 'nabil@example.com', 37, 46, NULL),
+('Rafi', 24, 'rafi@example.com', 41, 40, NULL),
+('Sophia', 22, 'sophia@example.com', 50, 52, NULL),
+('Hasan', 23, 'hasan@gmail.com', 43, 39, NULL);
