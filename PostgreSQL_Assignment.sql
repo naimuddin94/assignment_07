d1ba1a8098781576b8692840cdb15a275e0c7cd9
naimuddin94 <developernaimuddin@gmail.com>
2024-10-22 22:39:29 +0600

    Create students table

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -1,3 +1,16 @@
 -- Active: 1729184422674@@127.0.0.1@5432@postgres
--- create a database
+-- Create university database
 CREATE DATABASE university_db;
+
+
+-- Create students table
+CREATE TABLE students(
+    student_id SERIAL PRIMARY KEY,
+    student_name VARCHAR(50) NOT NULL,
+    age INT NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    frontend_mark INT,
+    backend_mark INT,
+    status VARCHAR(50)
+);
+
