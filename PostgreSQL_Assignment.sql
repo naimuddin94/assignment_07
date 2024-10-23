730d72e7a7ee8d5abc41eb5ac57d5e27519d5ece
naimuddin94 <developernaimuddin@gmail.com>
2024-10-23 16:47:50 +0600

    Query one and two complete

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -26,18 +26,19 @@ CREATE TABLE courses(
 CREATE TABLE enrollment(
     enrollment_id SERIAL PRIMARY KEY,
     student_id INT REFERENCES students(student_id) ON DELETE CASCADE NOT NULL,
-    course_id INT REFERENCES courses(course_id) ON DELETE CASCADE NOT NULL
+    course_id INT REFERENCES courses(course_id) ON DELETE CASCADE NOT NULL,
+    created_at TIMESTAMP DEFAULT now()
 );
 
 -- Insert sample data into the students table
-INSERT INTO students (student_name, age, email, frontend_mark, backend_mark, status)
+INSERT INTO students (student_name, age, email, frontend_mark, backend_mark)
 VALUES
-('Sameer', 21, 'sameer@example.com', 48, 60, NULL),
-('Zoya', 23, 'zoya@example.com', 52, 58, NULL),
-('Nabil', 22, 'nabil@example.com', 37, 46, NULL),
-('Rafi', 24, 'rafi@example.com', 41, 40, NULL),
-('Sophia', 22, 'sophia@example.com', 50, 52, NULL),
-('Hasan', 23, 'hasan@gmail.com', 43, 39, NULL);
+('Sameer', 21, 'sameer@example.com', 48, 60),
+('Zoya', 23, 'zoya@example.com', 52, 58),
+('Nabil', 22, 'nabil@example.com', 37, 46),
+('Rafi', 24, 'rafi@example.com', 41, 40),
+('Sophia', 22, 'sophia@example.com', 50, 52),
+('Hasan', 23, 'hasan@gmail.com', 43, 39);
 
 
 -- Insert sample data into the courses table
@@ -46,4 +47,36 @@ VALUES
 ('Next.js', 3),
 ('React.js', 4),
 ('Databases', 3),
-('Prisma', 3)
+('Prisma', 3);
+
+
+-- Insert sample data into the enrollment table
+INSERT INTO enrollment (student_id, course_id)
+VALUES
+(1, 1),
+(1, 2),
+(2, 1),
+(3, 2);
+
+-- Check student enrollment
+SELECT students.student_id,student_name, email, course_name, credits, created_at
+FROM enrollment
+INNER JOIN students ON enrollment.student_id = students.student_id
+INNER JOIN courses ON enrollment.course_id = courses.course_id;
+
+-- Assignment questions started
+
+-- Question number 01
+-- Insert a new student record with the following details!
+INSERT INTO students (student_name, age, email, frontend_mark, backend_mark, status)
+VALUES
+('MD Naim Uddin', 29, 'iamnaimuddin@yahoo.com', 100, 100, NULL);
+
+
+
+-- Question number 02
+-- Retrieve the names of all students who are enrolled in the course titled 'Next.js'.
+SELECT student_name FROM enrollment 
+JOIN courses ON enrollment.course_id = courses.course_id
+JOIN students ON enrollment.student_id = students.student_id
+WHERE course_name = 'Next.js';
